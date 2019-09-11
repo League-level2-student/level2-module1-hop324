3c8592aa7ebb77ffc95796803242186452da24d5
League Student <league@iMac-20.attlocal.net>
2019-09-10 18:27:34 -0700

    Guestbook, Intro To Array Lists
    :sunglasses: :100: :+1:

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,22 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class GuestBook implements ActionListener {
+	JFrame frame = new JFrame();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	
+	ArrayList<String> guestList = new ArrayList<String>();
+	
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +25,31 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	public static void main(String [] args) {
+		new GuestBook().createUI();
+	}
+	
+	
+	  void createUI() {
+		button1.setText("Add Name");
+		button2.setText("View Guests");
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		frame.setLayout(new BorderLayout());
+		frame.add(button1, BorderLayout.LINE_START); frame.add(button2, BorderLayout.LINE_END);
+		frame.setSize(100, 100);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == button1) {
+			String guest = JOptionPane.showInputDialog("What is the name to be added?");
+			guestList.add(guest);
+		}
+		else if(e.getSource() == button2) {
+			System.out.println(guestList);
+		}
+		
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,18 +1,35 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> Yeet = new ArrayList<String>();
 		//2. Add five Strings to your list
+		Yeet.add("My");
+		Yeet.add("name");
+		Yeet.add("is");
+		Yeet.add("Jeff");
+		Yeet.add(".");
 		
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = Yeet.size(); i > 0; i--) {
+			String j = Yeet.get(i);
+			System.out.println(j);
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(String k : Yeet){
+			System.out.println(k);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for(int h = Yeet.size(); h > 0; h--) {
+			if(h%2 == 0) {
+				String o = Yeet.get(h);
+				System.out.println(o);
+			}
+		}
 		//6. Print all the Strings in reverse order.
 		
 		//7. Print only the Strings that have the letter 'e' in them.
